2ef6fb8b22626160a4d33fbed6614363cd5fa157
VladBlonskyi <vladblonskyi@gmail.com>
2025-06-27 17:50:49 +0300

    Eslint config Setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,46 @@
-import js from '@eslint/js';
-import globals from 'globals';
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import prettierPlugin from 'eslint-plugin-prettier';
+import { fileURLToPath } from 'url';
 
-export default defineConfig([
+export default tseslint.config(
+  eslint.configs.recommended,
+
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    plugins: { js },
-    extends: ['js/recommended'],
+    files: ['**/*.ts', '**/*.tsx'],
+    ...tseslint.configs.recommendedTypeChecked[0],
+    languageOptions: {
+      globals: globals.node,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: fileURLToPath(new URL('.', import.meta.url)),
+      },
+    },
   },
+
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    languageOptions: { globals: globals.browser },
+    files: ['**/*.ts', '**/*.tsx'],
+    ...tseslint.configs.stylisticTypeChecked[0],
   },
-  tseslint.configs.recommended,
-]);
+
+  {
+    files: ['**/*.js', '**/*.mjs'],
+    languageOptions: {
+      globals: globals.node,
+    },
+    ...eslint.configs.recommended,
+  },
+
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  }
+);
