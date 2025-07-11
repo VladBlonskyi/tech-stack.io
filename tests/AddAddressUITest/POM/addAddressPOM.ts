f75be2d41b5ac1aabb03ac463480d4d8f424e1c3
VladBlonskyi <vladblonskyi@gmail.com>
2025-07-11 15:08:37 +0300

    Add Steps Pattern

--- tests/AddAddressUITest/POM/addAddressPOM.ts
+++ tests/AddAddressUITest/POM/addAddressPOM.ts
@@ -1,5 +1,4 @@
-import { Page, Locator, expect } from '@playwright/test';
-import { AddAddressDTO } from '../DTO/addAddressDTO';
+import { Page, Locator } from '@playwright/test';
 
 export class AddAddressPOM {
   readonly page: Page;
@@ -12,9 +11,8 @@ export class AddAddressPOM {
   readonly cityError: Locator;
   readonly stateError: Locator;
   readonly zipCodeError: Locator;
-  readonly deleteFirstAddress: Locator;
-  readonly deleteSecondAddress: Locator;
   readonly acceptDelete: Locator;
+  readonly addAddressPageLocator: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -27,65 +25,45 @@ export class AddAddressPOM {
     this.cityError = page.getByText('City is required');
     this.stateError = page.getByText('State is required');
     this.zipCodeError = page.getByText('Zip Code is required');
-    this.deleteFirstAddress = page
-      .locator('tr:has-text("Bylachovskogo")')
-      .getByTestId('button-Delete');
-    this.deleteSecondAddress = page
-      .locator('tr:has-text("Peremoga")')
-      .getByTestId('button-Delete');
     this.acceptDelete = page.getByTestId('button-Yes');
+    this.addAddressPageLocator = page.locator("//h1[text()='Add Address']");
   }
   async addAddressPageOpen() {
     await this.page.goto('/Forms/AddAddress');
   }
-  async fillAddressForm(address: AddAddressDTO) {
-    await this.streetAddressInput.fill(address.streetAddress);
-    await this.cityInput.fill(address.city);
-    await this.stateInput.fill(address.state);
-    await this.zipCodeInput.fill(address.zipCode);
+  async fillStreetAddressField(name: string) {
+    await this.streetAddressInput.fill(name);
+  }
+  async fillCityField(name: string) {
+    await this.cityInput.fill(name);
+  }
+  async fillStateField(name: string) {
+    await this.stateInput.fill(name);
+  }
+  async fillZipCodeField(name: string) {
+    await this.zipCodeInput.fill(name);
+  }
+  async clickCreateButton() {
     await this.createButton.click();
   }
-  async deleteAddress() {
-    if (await this.deleteFirstAddress.isVisible()) {
-      await this.deleteFirstAddress.click();
-      await this.acceptDelete.click();
-    }
-    if (await this.deleteSecondAddress.isVisible()) {
-      await this.deleteSecondAddress.click();
-      await this.acceptDelete.click();
-    }
+  getStreetAddressError() {
+    return this.streetAddressError;
   }
-  async getStreetAddressError() {
-    await expect(this.streetAddressError).toHaveText(
-      'Street Address is required'
-    );
+  getCityError() {
+    return this.cityError;
   }
-  async getCityError() {
-    await expect(this.cityError).toHaveText('City is required');
+  getStateError() {
+    return this.stateError;
   }
-  async getStateError() {
-    await expect(this.stateError).toHaveText('State is required');
+  getZipCodeError() {
+    return this.zipCodeError;
   }
-  async getZipCodeError() {
-    await expect(this.zipCodeError).toHaveText('Zip Code is required');
+  getDeleteButtonByStreet(streetName: string) {
+    return this.page
+      .locator(`tr:has-text("${streetName}")`)
+      .getByTestId('button-Delete');
   }
-
-  async checkExpectedError(field: string, expectedText: string) {
-    switch (field) {
-      case 'streetAddress':
-        await expect(this.streetAddressError).toHaveText(expectedText);
-        break;
-      case 'city':
-        await expect(this.cityError).toHaveText(expectedText);
-        break;
-      case 'state':
-        await expect(this.stateError).toHaveText(expectedText);
-        break;
-      case 'zipCode':
-        await expect(this.zipCodeError).toHaveText(expectedText);
-        break;
-      default:
-        throw new Error(`Unknown field: ${field}`);
-    }
+  async acceptDeleteClick() {
+    await this.acceptDelete.click();
   }
 }
--- tests/AddAddressUITest/addAddressUITest.spec.ts
+++ tests/AddAddressUITest/addAddressUITest.spec.ts
@@ -1,38 +1,28 @@
 import { test } from '@playwright/test';
-import { AddAddressPOM } from './POM/addAddressPOM';
 import { validTestData, invalidTestData } from './TestData/testDataAdress';
+import { AddAddressSteps } from './Steps/addAddresssteps';
+
+let addAddressSteps: AddAddressSteps;
+
+test.beforeEach(async ({ page }) => {
+  addAddressSteps = new AddAddressSteps(page);
+  await addAddressSteps.addAddressOpen();
+});
 
 test.describe('Add Address Positive Scenario', () => {
-  let addAddressPOM: AddAddressPOM;
-
-  test.beforeEach(async ({ page }) => {
-    addAddressPOM = new AddAddressPOM(page);
-    await addAddressPOM.addAddressPageOpen();
-  });
-  test.afterEach(async ({ page }) => {
-    await page.goto('/');
-    await addAddressPOM.deleteAddress();
-  });
-
   for (const data of validTestData) {
-    test(`Add address: ${data.streetAddress}`, async ({}) => {
-      await addAddressPOM.fillAddressForm(data);
+    test(`Check that Address can be added: ${data.streetAddress}`, async () => {
+      await addAddressSteps.fillAddressForm(data);
+      await addAddressSteps.deleteAddedAddress(data.streetAddress);
     });
   }
 });
 
 test.describe('Add Address Negative Scenario', () => {
-  let addAddressPOM: AddAddressPOM;
-
-  test.beforeEach(async ({ page }) => {
-    addAddressPOM = new AddAddressPOM(page);
-    await addAddressPOM.addAddressPageOpen();
-  });
-
   for (const data of invalidTestData) {
-    test(`Negative Test: missing ${data.expectedErrorField}`, async () => {
-      await addAddressPOM.fillAddressForm(data);
-      await addAddressPOM.checkExpectedError(
+    test(`Check that field ${data.expectedErrorField} is required`, async () => {
+      await addAddressSteps.fillAddressForm(data);
+      await addAddressSteps.checkExpectedError(
         data.expectedErrorField,
         data.expectedErrorText
       );
