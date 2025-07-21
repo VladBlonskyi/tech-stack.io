dbf07702c7f09e8888c574f7c62ba8003fa80c5a
VladBlonskyi <vladblonskyi@gmail.com>
2025-07-22 00:59:10 +0300

    Fix Api test

--- tests/Test/API/apiTestWithPlaywright.spec.ts
+++ tests/Test/API/apiTestWithPlaywright.spec.ts
@@ -1,27 +1,58 @@
 import { test, expect } from '@playwright/test';
 import { UserApiSteps } from '../../Steps/apiSteps';
+import { UserFactory } from '../../Factory/userFactory';
+import { UserDTO } from '../../DTO/userDTO';
 
 test.describe('Full user flow scenario', () => {
   let userSteps: UserApiSteps;
 
   test.beforeEach(async ({ request }) => {
     userSteps = new UserApiSteps(request);
-    await userSteps.createUser();
+    const response = await userSteps.createUser();
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);
   });
 
-  test.afterEach(async ({}) => {
-    await userSteps.deleteUser();
+  test.afterEach(async () => {
+    const deleteResponse = await userSteps.deleteUser();
+    expect(deleteResponse.ok()).toBeTruthy();
+    expect(deleteResponse.status()).toBe(200);
   });
 
-  test('Get info about user', async ({}) => {
-    await userSteps.getUserInfo();
+  test('Get info about user', async () => {
+    const response = await userSteps.getUserInfo();
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);
+    const user: UserDTO = await response.json();
+    expect(user.id).toBe(userSteps.createdUser.id);
+    expect(user.name).toBe(userSteps.createdUser.name);
+    expect(user.yearOfBirth).toBe(userSteps.createdUser.yearOfBirth);
   });
 
-  test('Update user', async ({}) => {
-    await userSteps.updateUser();
+  test('Update user', async () => {
+    const response = await userSteps.updateUser();
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);
+    const updatedUser = await response.json();
+    const expected = UserFactory.createNewUser('Female_Gender');
+    expect(updatedUser.name).toBe(expected.name);
+    expect(updatedUser.gender).toBe(expected.gender);
+    expect(updatedUser.yearOfBirth).toBe(expected.yearOfBirth);
   });
 
-  test('GET info about all users', async ({}) => {
-    await userSteps.getInfoAboutUsers();
+  test('Get info about all users', async () => {
+    const response = await userSteps.getAllUsers();
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);
+    const users: UserDTO[] = await response.json();
+    expect(Array.isArray(users)).toBe(true);
+    const found = users.find(
+      (user: UserDTO) => user.id === userSteps.createdUser.id
+    );
+    expect(found).toBeTruthy();
+    expect(found!.id).toBe(userSteps.createdUser.id);
+    expect(found!.name).toBe(userSteps.createdUser.name);
+    expect(found!.yearOfBirth).toBe(userSteps.createdUser.yearOfBirth);
+    expect(found!.gender).toBe(userSteps.createdUser.gender);
   });
 });
